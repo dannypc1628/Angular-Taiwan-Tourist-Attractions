737b65710d4917967cd72413f678c9e4b41533ad
dannyLiu <h152074716@gmail.com>
2022-02-19 18:48:59 +0800

    data.service 加入取得景點的API，定義 viewPoint 介面

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { ViewPoint } from './../interfaces/viewPoint';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import jsSHA from 'jssha';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +13,25 @@ export class DataService {
 
   getHotEvents() {
     return this.http.get('./assets/hotEvents.json');
+  }
+
+  getHotViewPoints(): Observable<ViewPoint[]> {
+    return this.http.get<ViewPoint[]>('https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/?$top=12&$format=JSON', {
+      headers: new HttpHeaders(this.GetAuthorizationHeader())
+    });
+  }
+
+  private GetAuthorizationHeader() {
+    var AppID = '670de1172e32459cbb1730bbb90f88d4';
+    var AppKey = 'gidxoY-nP55pqDWTVpYwYcJDbVI';
+
+    var GMTString = new Date().toUTCString();
+    var ShaObj = new jsSHA('SHA-1', 'TEXT');
+    ShaObj.setHMACKey(AppKey, 'TEXT');
+    ShaObj.update('x-date: ' + GMTString);
+    var HMAC = ShaObj.getHMAC('B64');
+    var Authorization = 'hmac username=\"' + AppID + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
+
+    return { 'Authorization': Authorization, 'X-Date': GMTString /*,'Accept-Encoding': 'gzip'*/ }; //如果要將js運行在伺服器，可額外加入 'Accept-Encoding': 'gzip'，要求壓縮以減少網路傳輸資料量
   }
 }
